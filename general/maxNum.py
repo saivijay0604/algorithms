e75a1af420c446ef7b02e005849f58d79f234cf6
saivijay0604 <saivijay0604@gmail.com>
2020-05-20 14:32:41 -0500

    rename of arrayDiv3

---
+++ general/maxNum.py
@@ -0,0 +1,16 @@
+from numpy import *
+def maxNumber(x,y):
+    max_num= max(arr)
+    print("max number from the array:",max_num)
+    if max_num >= num :
+        print("max number is:",max_num)
+        return True
+    else:
+        print ("max number is:", num)
+        return False
+
+arr = array([1,2,3,4,5,6])
+print("list of numbers: ", arr)
+num = int(input("Enter the number:"))
+maxNumber(arr,num)
+
